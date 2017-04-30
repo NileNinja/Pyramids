ac5de2b87152c7ca307e06e7616ab11510ef2dc3
Azazy <mazazy@lynks.com>
2017-04-30 19:44:45 +0200

    Left-header and first card implmentation.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+
+// Grade settings.
+
+window.addEventListener('load', function(){
+    Grade(document.querySelectorAll('.gradient-wrap'));
+    $('.gredclone').copyCSS('.gradient-wrap', ['background-image']);
+    $('div.gradient-wrap').css({'background-image':''});
+});
